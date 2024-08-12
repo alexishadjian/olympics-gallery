fdbfbe72c6c6dc903ccb554e101c933b166082c2
Alexis <alexishadjian@gmail.com>
2024-08-12 02:49:56 +0200

    feat: add favicon (theme based color)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,25 @@
+//FAVICONS
+import { FaviconLight, FaviconDark } from '@/public/index.js';
+
 //METADATA
 import type { Metadata } from "next";
 export const metadata: Metadata = {
     title: "Olympics gallery",
     description: "3D pictures gallery",
+    icons: [
+        {
+          rel: "icon",
+          type: "image/png",
+          media: "(prefers-color-scheme: light)",
+          url: FaviconLight.src,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          media: "(prefers-color-scheme: dark)",
+          url: FaviconDark.src,
+        },
+      ],
 };
 
 //FONT
@@ -13,7 +30,7 @@ const inter = Inter({ subsets: ["latin"] });
 import "./_styles/global.scss";
 
 //HEADER
-import Header from './_components/header/header';
+import Header from "./_components/header/header";
 import Footer from "./_components/footer/footer";
 
 
--- public/index.js
+++ public/index.js
@@ -2,7 +2,13 @@ import OlympicsLogoAnimated from './olmypics-logo-animated.svg';
 import OlympicsLogo from './olympics-logo.png';
 import OlympicsBgLogo from './olympics-bgLogo.svg';
 
+import FaviconDark from './favicon-dark.png';
+import FaviconLight from './favicon-light.png';
+
+
 export {
+    FaviconDark,
+    FaviconLight,
     OlympicsLogoAnimated,
     OlympicsLogo,
     OlympicsBgLogo
