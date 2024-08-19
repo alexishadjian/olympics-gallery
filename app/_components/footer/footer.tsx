db157526c1f614a5f98a1811b589fe5c6f1c9c1a
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-08-19 22:53:19 +0200

    content: add images

--- app/_components/footer/footer.tsx
+++ app/_components/footer/footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="gap-s">
-            <p className="text">28 août 2024</p>
-            <p className="text">__ 8 septembre 2024</p>
+            <p className="text">26 juillet 2024</p>
+            <p className="text">__ 11 août 2024</p>
             <Link href="https://www.instagram.com/paris2024" className="flex center link" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="1.5"  strokeLinecap="round"  strokeLinejoin="round">
                     <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
